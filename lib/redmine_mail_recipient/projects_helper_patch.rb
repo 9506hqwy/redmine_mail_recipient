995e8868f8f684fa87e896121b27fdeb277803ef
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-11-29 00:01:45 +0900

    Redmine3 で他のプラグインが ProjectsController を拡張した場合にプロジェクトの設定が表示できない問題を修正した。

--- lib/redmine_mail_recipient/projects_helper_patch.rb
+++ lib/redmine_mail_recipient/projects_helper_patch.rb
@@ -2,7 +2,7 @@
 
 module RedmineMailRecipient
   module ProjectsHelperPatch
-    def mail_recipient_setting_tabs(tabs)
+    def project_settings_tabs
       action = {
         name: 'mail_recipient',
         controller: :mail_recipients,
@@ -11,6 +11,7 @@ module RedmineMailRecipient
         label: :mail_recipient,
       }
 
+      tabs = super
       tabs << action if User.current.allowed_to?(action, @project)
       tabs
     end
@@ -23,34 +24,8 @@ module RedmineMailRecipient
       return false
     end
   end
-
-  module ProjectsHelperPatch4
-    include ProjectsHelperPatch
-
-    def self.included(base)
-      base.class_eval do
-        alias_method_chain(:project_settings_tabs, :mail_recipient)
-      end
-    end
-
-    def project_settings_tabs_with_mail_recipient
-      mail_recipient_setting_tabs(project_settings_tabs_without_mail_recipient)
-    end
-  end
-
-  module ProjectsHelperPatch5
-    include ProjectsHelperPatch
-
-    def project_settings_tabs
-      mail_recipient_setting_tabs(super)
-    end
-  end
 end
 
-if ActiveSupport::VERSION::MAJOR >= 5
-  Rails.application.config.after_initialize do
-    ProjectsController.send(:helper, RedmineMailRecipient::ProjectsHelperPatch5)
-  end
-else
-  ProjectsHelper.include RedmineMailRecipient::ProjectsHelperPatch4
+Rails.application.config.after_initialize do
+  ProjectsController.send(:helper, RedmineMailRecipient::ProjectsHelperPatch)
 end
