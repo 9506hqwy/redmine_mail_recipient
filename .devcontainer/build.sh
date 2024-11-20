dfef7ff034b329af105c4a99f5c1174bfeff5ee2
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-11-20 18:59:14 +0900

    template_redmine_plugin リポジトリの設定を追加した。

---
+++ .devcontainer/build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+REDMINE_URL=https://github.com/redmine/redmine.git
+MAIL_COMPAT3_URL=https://github.com/9506hqwy/redmine_mail_delivery_compat3.git
+
+sudo apt update
+sudo apt install -y imagemagick
+
+sudo mkdir "${REDMINE_HOME}"
+sudo chmod 777 "${REDMINE_HOME}"
+
+pushd "${REDMINE_HOME}"
+
+git clone --depth 1 -b 5.0-stable "${REDMINE_URL}" 5.0
+git clone --depth 1 -b 5.1-stable "${REDMINE_URL}" 5.1
+git clone --depth 1 -b 6.0-stable "${REDMINE_URL}" 6.0
+
+for BASE in $(ls)
+do
+    pushd "${BASE}"
+
+    cat >config/database.yml <<EOF
+production:
+  adapter: sqlite3
+  database: db/redmine.sqlite
+
+development:
+  adapter: sqlite3
+  database: db/redmine_dev.sqlite
+
+test:
+  adapter: sqlite3
+  database: db/redmine_test.sqlite3
+EOF
+
+    echo "gem 'debug'" > Gemfile.local
+
+    pushd ./plugins
+    git clone --depth 1 "${MAIL_COMPAT3_URL}"
+    popd
+
+    bundle install --with development test
+    bundle exec rake generate_secret_token
+    bundle exec rake db:migrate
+    echo ja | bundle exec rake redmine:load_default_data
+
+    popd
+done
--- README.md
+++ README.md
@@ -101,7 +101,8 @@ Recipient keyword is bellow.
   * 4.2
   * 5.0
   * 5.1
+  * 6.0
 * Database
   * SQLite
-  * MySQL 5.7
+  * MySQL 5.7 or 8.0
   * PostgreSQL 12
