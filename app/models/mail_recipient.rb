0e7dc75a7c9ac55abe10d6412443ca9694f9da05
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2024-11-20 19:19:22 +0900

    Redmine 6.0 を追加した。

--- app/models/mail_recipient.rb
+++ app/models/mail_recipient.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MailRecipient < ActiveRecord::Base
+class MailRecipient < RedmineMailRecipient::Utils::ModelBase
   belongs_to :project
   belongs_to :tracker
 
